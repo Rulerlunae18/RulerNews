cf26bc994b46d37072d35b85629736b1da6aa714
Rulerlunae18 <sivenukolga@gmail.com>
2025-05-03 00:53:32 +0300

    Create Users.java

---
+++ src/main/java/com/example/model/Users.java
@@ -0,0 +1,29 @@
+package com.example.models;
+
+public class User {
+    private String username;
+    private String passwordHash;
+    private boolean isVerified;
+    private boolean isAdmin;
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPasswordHash() { return passwordHash; }
+    public void setPasswordHash(String passwordHash) { this.passwordHash = passwordHash; }
+
+    public boolean isVerified() { return isVerified; }
+    public void setVerified(boolean verified) { isVerified = verified; }
+
+    public boolean isAdmin() { return isAdmin; }
+    public void setAdmin(boolean admin) { isAdmin = admin; }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", isVerified=" + isVerified +
+                ", isAdmin=" + isAdmin +
+                '}';
+    }
+}
