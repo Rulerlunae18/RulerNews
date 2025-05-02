118f0f5868e21bc78138d50449dd62c9dc0f2c52
Rulerlunae18 <sivenukolga@gmail.com>
2025-05-02 23:59:25 +0300

    Update DBConnection.java

--- src/main/java/com/example/utils/DBConnection.java
+++ src/main/java/com/example/utils/DBConnection.java
@@ -5,11 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String DB_URL = System.getenv("DB_URL");
+
+    private static final String DB_URL = "jdbc:mysql://" +
+            System.getenv("DB_HOST") + ":" +
+            System.getenv("DB_PORT") + "/" +
+            System.getenv("DB_NAME") + "?useSSL=false&serverTimezone=UTC";
     private static final String DB_USER = System.getenv("DB_USER");
     private static final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 }
