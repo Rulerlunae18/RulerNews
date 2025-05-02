8ad3308b1076cfdce9acca9724c6461add37267b
Rulerlunae18 <sivenukolga@gmail.com>
2025-05-03 00:59:07 +0300

    Update RegisterServlet.java

--- src/main/java/com/example/servlets/RegisterServlet.java
+++ src/main/java/com/example/servlets/RegisterServlet.java
@@ -1,5 +1,6 @@
 package com.example.servlets;
 
+import com.example.model.User;
 import com.example.utils.EmailService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
