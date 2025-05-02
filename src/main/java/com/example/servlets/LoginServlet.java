8637dcdc6476cac7354a3c3ebf50392a276eb904
Rulerlunae18 <sivenukolga@gmail.com>
2025-05-02 23:36:08 +0300

    Update LoginServlet.java

--- src/main/java/com/example/servlets/LoginServlet.java
+++ src/main/java/com/example/servlets/LoginServlet.java
@@ -10,9 +10,9 @@ import java.sql.*;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    private final String DB_URL = "jdbc:mysql://localhost:3306/news";
-    private final String DB_USER = "root";
-    private final String DB_PASSWORD = "RulerLovesYou";
+    private final String DB_URL = System.getenv("DB_URL");
+    private final String DB_USER = System.getenv("DB_USER");
+    private final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
