3b354b832eca02e09a3f88703a9ee5539800427c
Rulerlunae18 <sivenukolga@gmail.com>
2025-05-02 23:45:32 +0300

    Update DBConnection.java

--- src/main/java/com/example/utils/DBConnection.java
+++ src/main/java/com/example/utils/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/news";
-    private static final String USER = "root";
-    private static final String PASS = "RulerLovesYou";
+    private final String DB_URL = System.getenv("DB_URL");
+    private final String DB_USER = System.getenv("DB_USER");
+    private final String DB_PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
